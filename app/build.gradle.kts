526784182925e7e417964c6a8dffd924f9ed970e
mostafa nema rasheed <mostafa.n3ma@gmail.com>
2025-07-01 18:10:18 +0300

    add , CD deploy to firebase app distribution

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21"
+    alias(libs.plugins.firebase.appdistribution)
 }
 
 android {
